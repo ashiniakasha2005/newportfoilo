7e039177748305e1ba00c89a5e8aa055c2a6b752
Ashini Akasha <munigegaa.25@uom.lk>
2026-04-21 22:32:57 +0530

    Delete newportfoilo.js

--- newportfoilo.js
+++
@@ -1,6 +0,0 @@
-// Form submission handling
-document.getElementById('contactForm').addEventListener('submit', function(e) {
-  e.preventDefault(); // Prevent actual submission
-  document.getElementById('formMessage').textContent = "Thank you! Your message has been submitted.";
-  this.reset(); // Clear form fields
-});
